dbcb72a74eaa0b452f79251b6b15bc36d42f7075
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-05-09 00:56:27 +0900

    load画面

--- front/src/features/mailedit/components/EditForm.tsx
+++ front/src/features/mailedit/components/EditForm.tsx
@@ -9,22 +9,16 @@ import { useNavigate } from 'react-router-dom';
 const EditForm = (props: {
     questionId?: number
 }) => {
-	const navigate = useNavigate()
+	const navigate = useNavigate();
 	const [error, setError] = useState<string>('');
-
-	console.log("こっち側", props.questionId)
+	const [isLoad, setIsLoad] = useState(false);
 
 	const {
 		register, handleSubmit, formState: {errors}
 	} = useForm<UserAnser>()
 
 	const onSubmit = async (data: UserAnser) => {
-		console.log("送信データ", data)
-		if (props.questionId != undefined) {
-			console.log("これから入れる", props.questionId)
-			data.questionId = props.questionId
-		}
-		console.log("送信データ", data)
+		setIsLoad(true)
 		try {
 			const res = await fetch('https://busi-mail-dojo.onrender.com/answer', {
 				method: 'POST',
@@ -35,40 +29,53 @@ const EditForm = (props: {
 			const result = await res.json()
 			
 			if (res.status === 450) {
+				setIsLoad(false);
 				setError(result.error);
 				return;
 			}
 
-			console.log(result)
+			setIsLoad(false)
 			setError('');
 			navigate('/correct', { state: { result } });
 		} catch (error) {
+			setIsLoad(false);
 			console.log(error);
-			setError('エラーが発生しました。');
+			setError('エラーが発生しました。再度試して下さい。');
 		}
+		setIsLoad(false);
 	}
 
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<div className="h-1/2 px-4">
-				{error && (
-					<div className="text-red-500 text-sm mb-1">
-						{error}
+		<>
+			{isLoad && (
+				<div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+					<div className="bg-white p-4 rounded-lg shadow-lg">
+						<div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900 mx-auto"></div>
+						<p className="text-center mt-4">処理中...</p>
 					</div>
-				)}
-				
-				{/* <input type="hidden" value="0" {...register('questionId')} /> */}
-				<input type="hidden" value={props.questionId} {...register('questionId')} />
-				
-				<EditTitle register={register} errors={errors} />
-
-				<EditBody register={register} errors={errors} />
-				
-				<div className='flex justify-end lg:px-8 px-1'>
-					<button type="submit" className="bg-gray-500 hover:bg-gray-700 focus:bg-gray-700 text-white text-2xl m-3 px-4 py-2 rounded-md w-20 flex items-center justify-center transition-colors shadow-lg hover:shadow-xl focus:shadow-xl transform hover:-translate-y-1 focus:-translate-y-1 focus:outline-none"><MdSend /></button>
 				</div>
-			</div>
-		</form>
+			)}
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div className="h-1/2 px-4">
+					{error && (
+						<div className="text-red-500 text-sm mb-1">
+							{error}
+						</div>
+					)}
+					
+					{/* <input type="hidden" value="0" {...register('questionId')} /> */}
+					<input type="hidden" value={props.questionId} {...register('questionId')} />
+					
+					<EditTitle register={register} errors={errors} />
+
+					<EditBody register={register} errors={errors} />
+					
+					<div className='flex justify-end lg:px-8 px-1'>
+						<button type="submit" className="bg-gray-500 hover:bg-gray-700 focus:bg-gray-700 text-white text-2xl m-3 px-4 py-2 rounded-md w-20 flex items-center justify-center transition-colors shadow-lg hover:shadow-xl focus:shadow-xl transform hover:-translate-y-1 focus:-translate-y-1 focus:outline-none"><MdSend /></button>
+					</div>
+				</div>
+			</form>
+		</>
 	)
 }
 
